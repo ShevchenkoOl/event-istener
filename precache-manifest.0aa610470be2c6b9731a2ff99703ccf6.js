cb64b518b912d813e3c49c2a924b42f8dc9d6354
uzlabini@gmail.com <uzlabini@gmail.com>
2024-06-26 12:29:49 +0200

    Updates

---
+++ precache-manifest.0aa610470be2c6b9731a2ff99703ccf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fe4474128b11a17b4be873642ac1b8",
+    "url": "/event-istener/index.html"
+  },
+  {
+    "revision": "98ac027ffc8a8f1aafcf",
+    "url": "/event-istener/static/css/main.ada73c47.chunk.css"
+  },
+  {
+    "revision": "5ce5880426f1e26fb13c",
+    "url": "/event-istener/static/js/2.e47b2ed1.chunk.js"
+  },
+  {
+    "revision": "97b5938ef990e58844f0cff8514cc372",
+    "url": "/event-istener/static/js/2.e47b2ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ac027ffc8a8f1aafcf",
+    "url": "/event-istener/static/js/main.3b6339f3.chunk.js"
+  },
+  {
+    "revision": "65d2897bbf977cc9106f",
+    "url": "/event-istener/static/js/runtime-main.6d1fc480.js"
+  }
+]);
